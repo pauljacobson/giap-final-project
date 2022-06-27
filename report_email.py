d03b26a61d4c99cafdbd04ad6bb3905984339f2f
Paul Jacobson <pljcbsn@gmail.com>
2022-06-27 09:00:21 +0300

    In progress Trying to generate a report from the whole set of descriptions

--- report_email.py
+++ report_email.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python3
 
 import datetime
-from pathlib import Path
+import os
 from reports import generate_report
-from run import format_product_descriptions
+import run
+from emails import generate_email, send_email
 
 
-def generate_email():
+def main():
     """Generate an email with the processed report"""
     # Access the product descriptions
-    product_descriptions = format_product_descriptions()
+    json_product_descriptions, product_descriptions = run.format_product_descriptions()
     # Define the report path as /tmp/processed.pdf
-    report_file = Path("/tmp/processed.pdf")
+    report_file = os.path.join("./processed.pdf")
     # Specify the paragraph format for the product descriptions
-    paragraph = f"name: {product_descriptions['name']}\n weight: {product_descriptions['weight']}\n"
-    title = (
-        f"Processed Update on {format(datetime.datetime.now().strftime('%Y-%m-%d'))}"
-    )
+    title = (f"Processed Update on {format(datetime.datetime.now().strftime('%Y-%m-%d'))}")
 
-    # Generate the report
-    generate_report(paragraph, title, report_file)
+    # Loop over the product_descriptions dictionary
+    for key, value in product_descriptions.items():
+        # Add the product description to the paragraph
+        paragraph = f"name: {product_descriptions['name']}\n weight: {product_descriptions['weight']}\n"
+        
+
+    generate_report(report_file, title, paragraph) 
 
 
 if __name__ == "__main__":
-    generate_email()
+    main()
