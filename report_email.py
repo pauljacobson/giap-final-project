1386b28fcb19cdc23df97aa195d053851e17260a
Paul Jacobson <pljcbsn@gmail.com>
2022-07-17 09:16:20 +0300

    Adding process_data function to prepare the data

--- report_email.py
+++ report_email.py
@@ -7,19 +7,26 @@ import run
 from emails import generate_email, send_email
 
 
+def process_data(data):
+    """Process the data to be incorporated into the report"""
+    report = []
+    for item in data:
+        report.append("name: {}<br/>weight: {}\n".format(item[0], item[1]))
+    return report
+
+
 def main():
     """Generate an email with the processed report"""
-    # Access the product descriptions
+    # Import the source data to convert into a report
     json_product_descriptions, name_weight_list = run.format_product_descriptions()
-    print(name_weight_list)
+    # Generate summary data from a sorted list of items
+    summary = process_data(sorted(name_weight_list))
+    # Create a paragraph variable for the report
+    paragraph = "<br/><br/>".join(summary)
     # Define the report path as /tmp/processed.pdf
     report_file = os.path.join("./processed.pdf")
     # Specify the paragraph format for the product descriptions
     title = (f"Processed Update on {format(datetime.datetime.now().strftime('%Y-%m-%d'))}")
-    # Define the format of a paragraph in the report
-    for fruit in name_weight_list:
-        paragraph = (f"name: {name_weight_list[0]}, weight: {name_weight_list[1]}")
-
     # Generate the report
     generate_report(report_file, title, paragraph) 
 
