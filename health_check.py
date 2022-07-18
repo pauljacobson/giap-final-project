52fc01a37900edf3a87c5df7da8a1f7aa87787ac
Paul Jacobson <pljcbsn@gmail.com>
2022-07-18 09:31:37 +0300

    Add checks for system issues, and send emails Emails are sent if conditions are met

--- health_check.py
+++ health_check.py
@@ -48,7 +48,28 @@ def check_network_connection():
 
 def main():
     """Run system checks and email report"""
-    pass
+    body = 'Please check your system and resolve the issue as soon as possible.'
+    # Set attachment_path to None
+    attachment_path = None
+    # Check CPU usage
+    if check_cpu_usage() is True:
+        subject = "Error - CPU usage is over 80%"
+        send_email(generate_email("system@localhost", "admin@localhost", subject, body, attachment_path))
+
+    # Check available space
+    if check_available_space() is True:
+        subject = "Error - Available disk space is less than 20%"
+        send_email(generate_email("system@localhost", "admin@localhost", subject, body, attachment_path))
+
+    # Check available memory
+    if check_available_memory() is True:
+        subject = "Error - Available memory is less than 500MB"
+        send_email(generate_email("system@localhost", "admin@localhost", subject, body, attachment_path))
+
+    # Check network connection
+    if check_network_connection() is False:
+        subject = "Error - localhost cannot be resolved to 127.0.0.1"
+        send_email(generate_email("system@localhost", "admin@localhost", subject, body, attachment_path))
 
 
 if __name__ == "__main__":
